9a8a79323448b9fe3bf1d78d0951d79132b7c360
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-17 23:28:15 +0530

    1 . Update new Version

---
+++ SimpleSample/SimpleSample/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSample
+{
+    public class Model
+    {
+        public double OrderID { get; set; }
+
+        public string CustomerID { get; set; }
+
+        public int EmployeeID { get; set; }
+
+        public double Freight { get; set; }
+
+        public string ShipCity { get; set; }
+
+        public bool Verified { get; set; }
+
+        public string OrderDate { get; set; }
+
+        public string ShipName { get; set; }
+
+        public string ShipCountry { get; set; }
+
+        public string ShippedDate { get; set; }
+
+        public string ShipAddress { get; set; }
+
+    }
+}
---
+++ SimpleSample/SimpleSample/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SimpleSample
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+        private const string Url = "https://ej2services.syncfusion.com/production/web-services/api/Orders"; //This url is a free public api intended for demos
+        private readonly HttpClient _client = new HttpClient(); //Creating a new instance of HttpClient. (Microsoft.Net.Http)
+
+        private ObservableCollection<Model> data;
+
+        public ObservableCollection<Model> Data
+        {
+            get { return data; }
+            set
+            {
+                data = value;
+                OnPropertyChanged("Data");
+            }
+        }
+
+
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<Model>();
+            GetData();
+
+        }
+
+        async void GetData()
+        {
+            string content = await _client.GetStringAsync(Url); //Sends a GET request to the specified Uri and returns the response body as a string in an asynchronous operation
+            var json_Datas = JsonConvert.DeserializeObject<ObservableCollection<Model>>(content); //Deserializes or converts JSON String into a collection of Post
+            Data = json_Datas;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
